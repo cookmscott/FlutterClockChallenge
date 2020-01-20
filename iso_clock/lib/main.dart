03cd17593f7734517582b0c5b78e919e2cd0b541
unknown <cookmscott@gmail.com>
2020-01-20 11:08:31 -0500

    added zip of project for submission

--- iso_clock/lib/main.dart
+++ iso_clock/lib/main.dart
@@ -11,21 +11,10 @@ void main() {
   // A temporary measure until Platform supports web and TargetPlatform supports
   // macOS.
   if (!kIsWeb && Platform.isMacOS) {
-    // TODO(gspencergoog): Update this when TargetPlatform includes macOS.
     // https://github.com/flutter/flutter/issues/31366
     // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override.
     debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   }
-
-  // This creates a clock that enables you to customize it.
-  //
-  // The [ClockCustomizer] takes in a [ClockBuilder] that consists of:
-  //  - A clock widget (in this case, [DigitalClock])
-  //  - A model (provided to you by [ClockModel])
-  // For more information, see the flutter_clock_helper package.
-  //
-  // Your job is to edit [DigitalClock], or replace it with your
-  // own clock widget. (Look in digital_clock.dart for more details!)
   runApp(ClockCustomizer((ClockModel model) => IsoClock(model)));
 }
 
